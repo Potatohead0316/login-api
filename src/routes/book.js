deda1eb8022cb03839fc6010669265d2c6724841
Angelo S. Hagon <angelo.hagon@iloverbs.com>
2025-03-04 17:02:59 +0800

    update

--- src/routes/book.js
+++ src/routes/book.js
@@ -3,7 +3,6 @@ const router = express.Router()
 const Book = require('../models/Books')
 const { successResponse, errorResponse } = require('../helper/helper')
 
-// Add a new book
 router.post('/add-book', async (req, res) => {
     const { title, author, year, genre, image } = req.body
 
@@ -26,7 +25,6 @@ router.post('/add-book', async (req, res) => {
     }
 })
 
-// Get all books
 router.get('/list-books', async (req, res) => {
     try {
         const books = await Book.find()
@@ -35,5 +33,60 @@ router.get('/list-books', async (req, res) => {
         return errorResponse(res, 500, 'Internal server error')
     }
 })
+
+router.delete('/delete-book/:id', async (req, res) => {
+    const { id } = req.params
+    try {
+        const deletedBook = await Book.findByIdAndDelete(id)
+        if (deletedBook) {
+            return successResponse(res, 'Book deleted successfully', deletedBook)
+        } else {
+            return errorResponse(res, 404, 'Book not found')
+        }
+    } catch (error) {
+        return errorResponse(res, 500, 'Book not found')
+    }
+})
+
+router.put('/update-book/:id', async (req, res) => {
+    const { id } = req.params
+    const updateData = req.body  // Use all provided fields
+
+    try {
+        if (Object.keys(updateData).length === 0) {
+            return errorResponse(res, 400, 'No fields provided for update.')
+        }
+
+        const updatedBook = await Book.findByIdAndUpdate(id, updateData, { new: true })
+
+        if (updatedBook) {
+            return successResponse(res, 'Book updated successfully', updatedBook)
+        } else {
+            return errorResponse(res, 404, 'Book not found')
+        }
+    } catch (error) {
+        console.error('Error updating book:', error)
+        return errorResponse(res, 500, 'Internal server error')
+    }
+})
+
+router.get('/find-book/:id', async (req, res) => {
+    const { id } = req.params
+
+    try {
+        const book = await Book.findById(id)
+
+        if (book) {
+            return successResponse(res, 'Book retrieved successfully', book)
+        } else {
+            return errorResponse(res, 404, 'Book not found')
+        }
+    } catch (error) {
+        console.error('Error finding book:', error)
+        return errorResponse(res, 500, 'Internal server error')
+    }
+})
+
+
 
 module.exports = router
